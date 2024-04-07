189e39a3357fdfff44ed6f56260c915de1f24a4d
danyalahmad2000 <danyal.ahmad.ez@gmail.com>
2024-04-07 21:23:21 +0500

    First commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "../../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/*===== logo =====*/}
+          <div>
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: "50px", height: "60px" }}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
